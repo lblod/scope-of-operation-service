38ddafb286a8e5d6eaaea4d71e26c615c08efaf8
onodrim <alexander.van.den.berghe@redpencil.io>
2025-06-11 14:06:02 +0200

    chore: silence warning about unused argument

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 import { app, query, errorHandler } from 'mu';
-app.get("/", function (req, res) {
+app.get("/", function (_req, res) {
   res.send("Hello from scope-of-operation-service!");
 });
 
