67ce79053f90b143b164cdd336c593e3b31e78d7
onodrim <alexander.van.den.berghe@redpencil.io>
2025-06-16 15:47:22 +0200

    feat: identify location resources created by the service
    Extend the `prov:Location` resources created by this service with a
    `dcterms:creator` triple to identify them as having been created by this
    service. This allows to differentiate the ad-hoc resources created by this
    service from the, somewhat, standard set of scope of operation locations that is
    already defined.

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ export const PREFIX = {
   mu: "PREFIX mu: <http://mu.semte.ch/vocabularies/core/>",
   prov: "PREFIX prov: <http://www.w3.org/ns/prov#>",
   rdfs: "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+  dcterms: "PREFIX dcterms: <http://purl.org/dc/terms/>",
 };
 
 // NOTE (16/06/2025): We insert new location resources to the public graph as
@@ -12,3 +13,9 @@ export const PREFIX = {
 // the configuration of other services using this data, such as the producers,
 // allows sharing location resources between modules.
 export const locationGraph = "http://mu.semte.ch/graphs/public";
+
+// NOTE (16/06/2025): Used identify `prov:Location` resources created by this
+// service. This allows to easily differentiate "standard" locations that
+// already existed from the ad-hoc ones created by this service.
+export const creatorUri =
+  "http://lblod.data.gift/services/scope-of-operation-service";
